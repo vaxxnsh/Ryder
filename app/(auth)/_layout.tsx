2b8d6249ba1ba3e8dbfb7a5918263b0e3505bad6
vaxxnsh <kakshit817@gmail.com>
2025-08-17 09:28:31 +0530

    auth done

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+
+  return (
+      <Stack>
+        <Stack.Screen name="welcome" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      </Stack>
+  );
+}
---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,26 @@
+import { SignOutButton } from '@/components/SignOutButton'
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function Page() {
+  const { user } = useUser()
+
+  return (
+    <SafeAreaView>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
+    </SafeAreaView>
+  )
+}
